d1fa37ed0c36c58a888ba02f961696b87adbaa33
Alisha2012Alam <96848781+Alisha2012Alam@users.noreply.github.com>
2022-07-27 17:51:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.img.froURL('BirthdayImage.jpg', function(Img){
+        block_image_object.scaleToWidth(700)
+        block_image_object.scaleToWidth(510)
+        block_image_object.set({
+            top:0,
+            left:
+        })
+        canvas.add(block_image_object)
+    })
+	
+}
+
+function playSound(){
+	
+}
